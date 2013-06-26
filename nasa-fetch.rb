fc9d2d29b2891022625040419c139be51126d9a9
Bart Olsthoorn <bartolsthoorn@gmail.com>
2013-06-26 11:43:13 +0200

    Add timestamp to wallpaper to force change of wallpaper

--- nasa-fetch.rb
+++ nasa-fetch.rb
@@ -8,13 +8,15 @@ def open(url)
   Net::HTTP.get(URI.parse(url))
 end
 
+@timestamp = Time.now.to_i.to_s
+
 # Dirty, but hey, it's a hack, just plain Ruby, yeah!
 rss = open('http://www.nasa.gov/rss/lg_image_of_the_day.rss')
 image = rss.split('<url>http://www.nasa.gov/images/content/')[1].split('</url>')[0]
 url = "http://www.nasa.gov/images/content/#{image}"
 
-File.open("/Users/#@user/Pictures/nasa-wallpaper.jpg", 'w+') { |file| file.write(open(url)) }
+File.open("/Users/#@user/Pictures/nasa-wallpaper-#@timestamp.jpg", 'w+') { |file| file.write(open(url)) }
 
-`./nasa-set.sh /Users/#@user/Pictures/nasa-wallpaper.jpg`
+`./nasa-set.sh /Users/#@user/Pictures/nasa-wallpaper-#@timestamp.jpg`
 
 puts 'NASA Wallpaper of the day successfully landed'
